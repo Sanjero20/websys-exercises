738e0e3e78c394a00573f55c67180e1d790986e2
Sanjero <gelosantos72@gmail.com>
2022-11-18 22:55:02 +0800

    Prompt error if invalid DNA string

--- Part 2: Programming/app.js
+++ Part 2: Programming/app.js
@@ -23,3 +23,31 @@ function calcHammingDistance(dna1, dna2) {
 
   return mismatch;
 }
+
+const input1 = document.getElementById('DNA1');
+const input2 = document.getElementById('DNA2');
+
+const err1 = document.querySelector('.d1');
+const err2 = document.querySelector('.d2');
+
+const btn = document.getElementById('calc');
+
+input1.addEventListener('input', () => {
+  input1.value = input1.value.toUpperCase();
+  if (isValidDNA(input1.value) == false) {
+    err1.textContent = '* Not a valid DNA String *';
+  } else {
+    err1.textContent = '';
+  }
+});
+
+input2.addEventListener('input', () => {
+  input2.value = input2.value.toUpperCase();
+  if (isValidDNA(input2.value) == false) {
+    err2.textContent = '* Not a valid DNA String *';
+  } else {
+    err2.textContent = '';
+  }
+});
+
+btn.addEventListener('click', () => {});
